35e9909c0e118028fc9f345af9d72ae54faccc3f
Distanta Ojha <48418897+DistantaOjha@users.noreply.github.com>
2020-10-27 21:10:44 -0400

    Devices can get data from another device now and can estimate distance using rssi signal strength

--- app/src/main/java/com/prototype/gbcontacttracing/bluetoothManager/BleManager.kt
+++ app/src/main/java/com/prototype/gbcontacttracing/bluetoothManager/BleManager.kt
@@ -1,11 +1,14 @@
 package com.prototype.gbcontacttracing.bluetoothManager
+
 import android.app.Activity
 import android.bluetooth.BluetoothAdapter
 import android.bluetooth.BluetoothManager
-import android.bluetooth.le.BluetoothLeScanner
-import android.bluetooth.le.ScanCallback
-import android.bluetooth.le.ScanSettings
+import android.bluetooth.le.*
 import android.content.Context
+import android.os.ParcelUuid
+import android.util.Log
+import java.util.*
+
 
 class BleManager {
 
@@ -14,6 +17,9 @@ class BleManager {
         private lateinit var bluetoothManager: BluetoothManager
         private lateinit var bluetoothAdapter: BluetoothAdapter
         private lateinit var bleScanner: BluetoothLeScanner
+        private lateinit var bleAdvertiser: BluetoothLeAdvertiser
+
+
         var isScanning = false
 
         fun setBluetooth(activity: Activity) {
@@ -21,6 +27,7 @@ class BleManager {
                 activity.getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
             bluetoothAdapter = bluetoothManager.adapter
             bleScanner = bluetoothAdapter.bluetoothLeScanner
+            bleAdvertiser = bluetoothAdapter.bluetoothLeAdvertiser
         }
 
         fun startBleScan(scanSettings: ScanSettings, scanCallback: ScanCallback) {
@@ -40,6 +47,42 @@ class BleManager {
         fun getBleAdapter(): BluetoothAdapter {
             return bluetoothAdapter
         }
+
+        fun startAdvertising() {
+            val settings = AdvertiseSettings.Builder()
+                .setAdvertiseMode(AdvertiseSettings.ADVERTISE_MODE_LOW_POWER)
+                .setTxPowerLevel(AdvertiseSettings.ADVERTISE_TX_POWER_MEDIUM)
+                .setConnectable(false)
+                .build()
+
+
+            //TODO : add token to advertise here
+            val sixteenBit = ParcelUuid(UUID.fromString("00001234-0000-1000-8000-00805F9B34FB"))
+            Log.i("id is:", sixteenBit.toString())
+            val sendData = "hoang".toByteArray(Charsets.UTF_8)
+
+            Log.i("Given data is: bytes", sendData.toString())
+            val data = AdvertiseData.Builder()
+                .setIncludeDeviceName(false)
+                .setIncludeTxPowerLevel(true)
+                .addServiceData(sixteenBit, sendData)
+                .build()
+
+            val advertisingCallback: AdvertiseCallback = object : AdvertiseCallback() {
+                override fun onStartSuccess(settingsInEffect: AdvertiseSettings) {
+                    super.onStartSuccess(settingsInEffect)
+                    Log.i("BLE", "LE Advertise success.")
+                }
+
+                override fun onStartFailure(errorCode: Int) {
+                    Log.e("BLE", "Advertising onStartFailure: $errorCode")
+                    super.onStartFailure(errorCode)
+                }
+            }
+
+            bleAdvertiser.startAdvertising(settings, data, advertisingCallback)
+        }
+
     }
 
 
--- app/src/main/java/com/prototype/gbcontacttracing/ui/home/HomeFragment.kt
+++ app/src/main/java/com/prototype/gbcontacttracing/ui/home/HomeFragment.kt
@@ -3,6 +3,7 @@ import android.bluetooth.le.ScanCallback
 import android.bluetooth.le.ScanResult
 import android.bluetooth.le.ScanSettings
 import android.os.Bundle
+import android.os.ParcelUuid
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
@@ -11,22 +12,36 @@ import android.widget.Button
 import androidx.fragment.app.Fragment
 import com.prototype.gbcontacttracing.R
 import com.prototype.gbcontacttracing.bluetoothManager.BleManager
+import java.util.*
+import kotlin.math.pow
 
 
 class HomeFragment : Fragment() {
 
     private lateinit var inflatedView: View
     private val scanSettings = ScanSettings.Builder()
-        .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+        .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
         .build()
 
     private val scanCallback = object : ScanCallback() {
         override fun onScanResult(callbackType: Int, result: ScanResult) {
+            ScanResult.DATA_COMPLETE
+
             with(result.device) {
                 Log.d(
                     "Scan Callback",
-                    "Found BLE device! Name: ${name ?: "Unnamed"}, address: $address"
+                    "Found BLE device! Name: ${name ?: "Unnamed"}, address: $address, strength: ${result.rssi}"
                 )
+
+                val serviceData =
+                    result.scanRecord?.getServiceData(ParcelUuid(UUID.fromString("00001234-0000-1000-8000-00805F9B34FB")))
+
+                if (serviceData != null) {
+                    val string = String(serviceData)
+                    Log.d("We received data:", string)
+                    val distanceInFeets = 10.0.pow((-69 - (result.rssi)) / (10.0 * 2)) * 3.28084
+                    Log.d("At distance:", distanceInFeets.toString())
+                }
             }
         }
     }
@@ -43,6 +58,7 @@ class HomeFragment : Fragment() {
         val button = inflatedView.findViewById(R.id.scan_ble) as Button
         button.setOnClickListener {
             BleManager.startBleScan(scanSettings, scanCallback)
+            BleManager.startAdvertising()
             Log.d("Hanuman", "banuman pressed button")
         }
 
