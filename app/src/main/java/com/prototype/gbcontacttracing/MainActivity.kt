7d0f9a62abad12f7cb35f1f574fc93f7d12e3f5d
Paul Mogianesi <pjm16177@gmail.com>
2020-11-02 21:47:18 -0500

    Fixed Main Screen

--- app/src/main/java/com/prototype/gbcontacttracing/MainActivity.kt
+++ app/src/main/java/com/prototype/gbcontacttracing/MainActivity.kt
@@ -23,78 +23,7 @@ import com.prototype.gbcontacttracing.ui.infoPage.InfoFragment
 import com.prototype.gbcontacttracing.ui.release.ReleaseFragment
 import kotlinx.android.synthetic.main.activity_main.*
 
-private const val ENABLE_BLUETOOTH_REQUEST_CODE = 1
-private const val LOCATION_PERMISSION_REQUEST_CODE = 2
-
-
 class MainActivity : AppCompatActivity() {
-
-    private val bluetoothAdapter: BluetoothAdapter by lazy {
-        val bluetoothManager = getSystemService(Context.BLUETOOTH_SERVICE) as BluetoothManager
-        bluetoothManager.adapter
-    }
-
-    override fun onResume() {
-        super.onResume()
-        requestLocationPermission()
-        if (!bluetoothAdapter.isEnabled) {
-            promptEnableBluetooth()
-        }
-    }
-
-    private fun promptEnableBluetooth() {
-        if (!bluetoothAdapter.isEnabled) {
-            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-            startActivityForResult(enableBtIntent, ENABLE_BLUETOOTH_REQUEST_CODE)
-        }
-    }
-
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        when (requestCode) {
-            ENABLE_BLUETOOTH_REQUEST_CODE -> {
-                //If user denies the permission for bluetooth
-                if (resultCode != Activity.RESULT_OK) {
-                    promptEnableBluetooth()
-                }
-            }
-        }
-    }
-
-    private fun Activity.requestPermission(permission: String, requestCode: Int) {
-        ActivityCompat.requestPermissions(this, arrayOf(permission), requestCode)
-    }
-
-    private fun requestLocationPermission() {
-        if (isLocationPermissionGranted) {
-            print("permission allowed")
-            return
-        }
-        runOnUiThread{
-            val dialogBuilder = AlertDialog.Builder(this)
-            dialogBuilder.setTitle("Locations Permission Required")
-            dialogBuilder.setMessage("Location Permission is required in order for this app to run and use Bluetooth Low Energy Services")
-            dialogBuilder.setPositiveButton("Allow Location"
-            ) { _: DialogInterface, _: Int ->
-                requestPermission(
-                    Manifest.permission.ACCESS_FINE_LOCATION,
-                    LOCATION_PERMISSION_REQUEST_CODE
-                )
-            }
-
-            val alert = dialogBuilder.create()
-            alert.show()
-
-        }
-
-    }
-
-    private val isLocationPermissionGranted
-        get() = hasPermission(Manifest.permission.ACCESS_FINE_LOCATION)
-    private fun Context.hasPermission(permissionType: String): Boolean {
-        return ContextCompat.checkSelfPermission(this, permissionType) ==
-                PackageManager.PERMISSION_GRANTED
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
--- app/src/main/java/com/prototype/gbcontacttracing/bluetoothManager/BleManager.kt
+++
@@ -1,6 +0,0 @@
-package com.prototype.gbcontacttracing.bluetoothManager
-
-class BleManager {
-
-
-}
