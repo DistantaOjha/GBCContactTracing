53eba2358d6dcd7f958f108f4b455deb7ea741b5
Paul Mogianesi <pjm16177@gmail.com>
2020-11-10 21:32:55 -0500

    Updated UI

--- app/src/main/java/com/prototype/gbcontacttracing/AgeActivity.kt
+++ app/src/main/java/com/prototype/gbcontacttracing/AgeActivity.kt
@@ -3,6 +3,7 @@ package com.prototype.gbcontacttracing
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_age.*
 
 class AgeActivity : AppCompatActivity() {
--- app/src/main/java/com/prototype/gbcontacttracing/LoginActivity.kt
+++ app/src/main/java/com/prototype/gbcontacttracing/LoginActivity.kt
@@ -6,10 +6,10 @@ import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
         loginVerifyButton.setOnClickListener{
             val intent = Intent(this, IntroActivity::class.java)
             startActivity(intent)
