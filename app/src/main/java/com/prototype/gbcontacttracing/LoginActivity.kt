b03525146395f757b9112abb218a0d4fe005344d
Paul Mogianesi <pjm16177@gmail.com>
2020-11-15 18:45:03 -0500

    email verification

--- app/src/main/java/com/prototype/gbcontacttracing/LoginActivity.kt
+++ app/src/main/java/com/prototype/gbcontacttracing/LoginActivity.kt
@@ -1,9 +1,17 @@
 package com.prototype.gbcontacttracing
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONArray
+import java.io.BufferedReader
+import java.io.InputStreamReader
+import java.io.OutputStreamWriter
+import java.net.HttpURLConnection
+import java.net.URL
+import java.net.URLEncoder
 
 class LoginActivity : AppCompatActivity() {
 
@@ -11,8 +19,49 @@ class LoginActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
         loginVerifyButton.setOnClickListener{
+            val email = emailBox.text.toString()
+            val code = getRandomString(5)
+            val id = "196bcd9c-23c4-11eb-adc1-0242ac120002"
+            sendPostRequest(id, code, email)
             val intent = Intent(this, IntroActivity::class.java)
             startActivity(intent)
         }
+    }
+
+    fun sendPostRequest(id:String, password:String, email:String) {
+
+        var reqParam = URLEncoder.encode("id", "UTF-8") + "=" + URLEncoder.encode(id, "UTF-8")
+        reqParam += "&" + URLEncoder.encode("code", "UTF-8") + "=" + URLEncoder.encode(password, "UTF-8")
+        reqParam += "&" + URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8")
+        val mURL = URL("http://p4pproto.sites.gettysburg.edu/GBContactTracing/verify.php")
+
+        with(mURL.openConnection() as HttpURLConnection) {
+            requestMethod = "POST"
+
+            val wr = OutputStreamWriter(outputStream)
+            wr.write(reqParam)
+            wr.flush()
+
+            println("URL : $url")
+            println("Response Code : $responseCode")
+
+            BufferedReader(InputStreamReader(inputStream)).use {
+                val response = StringBuffer()
+
+                var inputLine = it.readLine()
+                while (inputLine != null) {
+                    response.append(inputLine)
+                    inputLine = it.readLine()
+                }
+                println("Response : $response")
+            }
+        }
+    }
+
+    private fun getRandomString(length: Int) : String {
+        val allowedChars = ('A'..'Z') + ('a'..'z') + ('0'..'9')
+        return (1..length)
+            .map { allowedChars.random() }
+            .joinToString("")
     }
 }
